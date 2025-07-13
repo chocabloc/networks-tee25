64a6f9496e5828b2c14b4fe70549326e9ea872e9
Sequoia-Dev <ndurga@iisc.ac.in>
2025-07-13 05:24:00 -0700

    Update1 model.py
    Corrected error that lead to trust update based on new opinions instead of old.

--- model.py
+++ model.py
@@ -10,23 +10,27 @@ class BabyModel(Model):
     def __init__(self, delta):
         self.delta = delta
 
-    def update_opinions(self, G):
-        new_opinions = {}
-        for i in G.nodes():
-            o_i = G.nodes[i]['opinion']
-            alpha_i = G.nodes[i]['agreeableness']
-            total_delta = 0
-            for j in G.neighbors(i):
-                o_j = G.nodes[j]['opinion']
-                trust_ij = G[i][j]['trust']
-                delta_ij = abs(o_i - o_j)
-                feedback = o_j if delta_ij <= self.delta else -o_j
-                total_delta += trust_ij * (feedback - o_i)
-            new_opinions[i] = o_i + alpha_i * total_delta
-        for i in G.nodes():
-            G.nodes[i]['opinion'] = np.clip(new_opinions[i], -1, 1)
+   def opinew(G)->list:
+    new_opinions = {}
+    for i in G.nodes():
+        o_i = G.nodes[i]['opinion']
+        alpha_i = G.nodes[i]['agreeableness']
+        total_delta = 0
+        for j in G.neighbors(i):
+            o_j = G.nodes[j]['opinion']
+            trust_ij = G[i][j]['trust']
+            delta_ij = abs(o_i - o_j)
+            feedback = o_j if delta_ij <= d else -o_j
+            total_delta += trust_ij * (feedback - o_i)
+        new_opinions[i] = o_i + alpha_i * total_delta
+     return new_opinions
 
-    def update_trust(self, G):
+    def update_opinions(G):
+        for i in G.nodes():
+            G.nodes[i]['opinion'] = np.clip(r[i], -1, 1)
+
+
+    def update_trust(G):
         for i in G.nodes():
             eta_i = G.nodes[i]['loyalty']
             o_i = G.nodes[i]['opinion']
@@ -37,6 +41,8 @@ class BabyModel(Model):
                 trust_new = trust_old + eta_i * (sim - trust_old)
                 G[i][j]['trust'] = np.clip(trust_new, 0, 1)
 
-    def step(self, g):
-        self.update_opinions(g)
-        self.update_trust(g)
+
+     def step(self, g):
+        r= self.opinew(G)
+        self.update_trust(G)
+        self.update_opinions(G)
