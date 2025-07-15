9fe2c96d3def272f0ae2ed3bacc496434f7865a1
Aditya Arsh <aarshisback@gmail.com>
2025-07-15 07:31:11 +0530

    more metrics

--- simulation.py
+++ simulation.py
@@ -11,32 +11,41 @@ class Simulation:
 
     def run(self, display_nth=0):
         og.plot_opinions(self.graph, f"Opinion distribution at step 0")
-        extn, extp, avg = [], [], []
+        extn, extp, pavg, navg, avg = [], [], [], [], []
         for i in tqdm(range(1, self.steps+1)):
             self.graph = self.model.step(self.graph)
 
             # calculate some metrics
-            nextn, nextp, total = 0, 0, 0
+            nextn, nextp, total, totalp, totaln = 0, 0, 0, 0, 0
             for j in self.graph.nodes():
                 op = self.graph.nodes[j]['opinion']
-                if op <= -0.95:
+                if op <= -0.75:
                     nextn += 1
-                elif op >= 0.95:
+                elif op >= 0.75:
                     nextp += 1
                 total += op
+                if op < 0:
+                    totaln += op
+                else:
+                    totalp += op
             total /= self.graph.number_of_nodes()
+            totalp /= self.graph.number_of_nodes()
+            totaln /= self.graph.number_of_nodes()
             extn.append(nextn)
             extp.append(nextp)
             avg.append(total)
+            pavg.append(totalp)
+            navg.append(totaln)
 
             if display_nth != 0 and i % display_nth == 0:
                 # print(f"person 0 has opinion {self.graph.nodes[0]['opinion']}")
                 og.plot_opinions(self.graph, f"Opinion distribution at step {i}")
 
-        plt.plot(extn)
         plt.plot(extp)
+        plt.plot(extn)
         plt.show()
-        plt.plot(avg)
+        plt.plot(pavg)
+        plt.plot(navg)
         plt.show()
         self.show_metrics()
 
